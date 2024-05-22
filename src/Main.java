520f24f0ed3014ada91871001e0c8606f0c6baf5
New <Dar1985@bk.ru>
2024-05-22 22:49:25 +0700

    ДЗ исправлено

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,23 @@ public class Main {
             String lastName = "Ivanov";
             String firstName = "Ivan";
             String middleName = "Ivanovich";
-            String fullName = lastName + "  " + firstName + "  " + middleName;
+            String fullName = lastName + " " + firstName + " " + middleName;
             System.out.println("Ф.И.О сотрудника - " + fullName);
+
         }
 
         {
             System.out.println("\nTask2");
             String fullName = "Ivanov Ivan Ivanovich";
             System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+
         }
         {
             System.out.println("\nTask3");
             String fullName = "Иванов Семён Семёнович";
             String replacedFullName = fullName.replace('ё', 'е');
             System.out.println("Данные Ф.И.О. сотрудника - " + replacedFullName);
+
         }
     }
 }
